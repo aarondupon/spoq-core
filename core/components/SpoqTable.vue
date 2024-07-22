2c357a15272dc2a0526a6ea25e2d5dbac07973e8
Aaron Dupon <adupon@deloitte.com>
2024-07-22 12:15:19 +0200

    comp table

---
+++ core/components/SpoqTable.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container mx-auto px-4 sm:px-8">
+    <div class="py-8">
+      <div class="flex flex-row mb-1 sm:mb-0 justify-between w-full">
+        <h2 class="text-2xl leading-tight">Demo Table</h2>
+      </div>
+      <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+        <div class="inline-block min-w-full shadow-md rounded-lg overflow-hidden">
+          <table class="min-w-full leading-normal">
+            <thead>
+              <tr>
+                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">Name</th>
+                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">Age</th>
+                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">Email</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="person in people" :key="person.email">
+                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">{{ person.name }}</td>
+                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">{{ person.age }}</td>
+                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">{{ person.email }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      people: [
+        { name: 'John Doe', age: 25, email: 'john@example.com' },
+        { name: 'Jane Smith', age: 30, email: 'jane@example.com' },
+        { name: 'Sam Brown', age: 22, email: 'sam@example.com' },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+  .container {
+    background-color: #ededed;
+  }
+</style>
